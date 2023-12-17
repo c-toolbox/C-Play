70a756611d3a0cad2d6a248e67bed2a32873b23b
eriksunden <eriksunden85@gmail.com>
2023-12-17 16:27:16 +0100

    Updated copyright text

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 /*
- * SPDX-FileCopyrightText: 2020 George Florea Bănuș <georgefb899@gmail.com>
+ * SPDX-FileCopyrightText: 
+ * 2021-2023 Erik Sundén <eriksunden85@gmail.com> 
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
